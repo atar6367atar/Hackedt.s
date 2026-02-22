d231fe48bd2fea468c0e1a095c1a2a57e6cbcd18
atar6367atar <atarpubg8@gmail.com>
2026-02-23 01:10:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Tüm dosyaları olduğu gibi serve et
-app.use(express.static(path.join(__dirname)));
+// Statik dosyaları serve et
+app.use(express.static(path.join(__dirname, './')));
 
-// Tüm yönlendirmeleri index.html'e yönlendir (SPA desteği)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// Ana sayfayı yönlendir
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server ${port} portunda çalışıyor`);
+app.listen(port, () => {
+    console.log(`Server ${port} portunda çalışıyor`);
 });
